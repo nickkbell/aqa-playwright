fea62a5ff723215a37e595edd297c8359aad8fb9
Nikita Belitskiy <35149662+nickkbell@users.noreply.github.com>
2024-09-02 17:54:49 +0300

    Homework 24 (#3)
    * homework_23
    
    * homework_24
    
    * homework_24
    
    ---------
    
    Co-authored-by: nick <n.belitskiy@infomir.com>

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const validUserData = {
+    name: 'John',
+    lastName: 'Snow',
+    email: `aqa_${Date.now()}@gmail.com`,
+    password: 'Password123!',
+    reEnterPassword: 'Password123!'
+};
